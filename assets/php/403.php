6608c6c559928536d244a17e034f835732724633
Saneem <xaneem@gmail.com>
2013-10-14 08:06:39 +0530

    Added asset files
    Bootstrap theme files, custom styles, and basic images.

---
+++ assets/php/403.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+	<title>403 Noob Found</title>
+	<style type="text/css">
+	::selection{ background-color: #E13300; color: white; }
+	::moz-selection{ background-color: #E13300; color: white; }
+	::webkit-selection{ background-color: #E13300; color: white; }
+	body {
+		background-color: #fff;
+		margin: 40px;
+		font: 13px/20px normal Helvetica, Arial, sans-serif;
+		color: #4F5155;
+	}
+	a {
+		color: #003399;
+		background-color: transparent;
+		font-weight: normal;
+	}
+	h1 {
+		color: #444;
+		background-color: transparent;
+		border-bottom: 1px solid #D0D0D0;
+		font-size: 19px;
+		font-weight: normal;
+		margin: 0 0 14px 0;
+		padding: 14px 15px 10px 15px;
+	}
+	code {
+		font-family: Consolas, Monaco, Courier New, Courier, monospace;
+		font-size: 12px;
+		background-color: #f9f9f9;
+		border: 1px solid #D0D0D0;
+		color: #002166;
+		display: block;
+		margin: 14px 0 14px 0;
+		padding: 12px 10px 12px 10px;
+	}
+	#container {
+		margin: 10px;
+		border: 1px solid #D0D0D0;
+		-webkit-box-shadow: 0 0 8px #D0D0D0;
+	}
+	p {
+		margin: 12px 15px 12px 15px;
+	}
+	</style>
+	<body>
+		<div id="container">
+			<h1>Error 403: Noob found!</h1>
+		<p>Don't be anxious about what's ahead. Get a pencil and a paper, and work your brains hard.</p>	
+		<br>
+		<p>Going this way won't take you any further.</p>
+	</div>
+</body>
+</html>
