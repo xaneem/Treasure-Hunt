929aba6017cfc29d6e8062c7eb250f9011e3ee8c
Saneem <xaneem@gmail.com>
2014-04-25 06:10:21 +0530

    Updated winners.php
    Removed reference to winner image.

--- application/views/pages/winners.php
+++ application/views/pages/winners.php
@@ -4,9 +4,8 @@
 		<h1>Chapter 1 <span class="text-muted">has winners!</span></h1>
 		<h2>xaneem <span class="text-muted">(Saneem, from NIT Calicut)</span> bags Rs. 10,000 ;)</h2>
 		<br>
-		<img src="/assets/img/winner.jpg" width="400" height="400"/>
-		<br>
 		<h2>Congratulations.</h2>
+		<br>
 		<h3>Now there's Chapter 2 to crack - only your brains can get you up here.</h3>
 	</div>
 </div>
